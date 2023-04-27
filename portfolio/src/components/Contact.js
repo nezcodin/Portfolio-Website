d02b42c1a4d0818b0d598c06ebe1764ad53f54e9
nezcodin <ch.fusco104@gmail.com>
2023-04-27 00:07:24 -0500

    contact page css

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,6 +1,12 @@
 export const Contact = () => {
   return (
-    <div>
+    <div className="contact">
+      <div className="contact-box">
+        <input placeholder="Subject" className="subject"></input>
+        <textarea placeholder="What do you want to say? Please mention your name and a good way to reach back out to you." className="message"></textarea>
+        <button type="submit">Send Email</button>
+      </div>
+      <p>Or send me a message over on <a href="https://www.linkedin.com/in/charlottefusco/" target="_blank" rel="noreferrer">LinkedIn</a>!</p>
     </div>
   )
 }
