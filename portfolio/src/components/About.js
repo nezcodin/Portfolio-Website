9eb1773de51553732b11ae62df94b78e29196e87
nezcodin <ch.fusco104@gmail.com>
2023-04-26 23:29:42 -0500

    styling + font changes

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -7,7 +7,7 @@ export const About = () => {
         <img src={profilePicture} alt='profile' />
       </div>
       <div className='about-text'>
-        <h2>Hey, thanks for stopping by!</h2>
+        <h2 className='about-title'>Hey, thanks for stopping by!</h2>
         <p>My name is Charlotte Fusco. As a software engineer with a customer service background, I excel in providing creative solutions to complex problems while continuously learning and staying up-to-date with the latest trends in tech. My expertise in teamwork and customer-focused problem-solving allows me to effectively identify and resolve customer issues.</p>
         <p>Languages and Frameworks: JavaScript | Python | HTML5 | CSS3 | SQL | React | Node.js | PostgreSQL | Express</p>
         <p>Technologies: Git | GitHub | RESTful API | MacOS | Linux | Adobe After Effects | QuickBooks | Figma | Microsoft Excel</p>
--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom"
+
 export const Footer = () => {
   return (
     <div className="footer">
-      <p className="contact-p">Contact Me</p>
-      <ul className="footer-link-container">
-        <li className="footer-link">Email: ch.fusco104@gmail.com</li>
-        <li className="footer-link">Phone: (319) 804-9186</li>
-        <li className="footer-link">LinkedIn</li>
-      </ul>
+      <div className="footer-info">
+        <h3 className="contact-p">
+          <Link to='/contact' className="contact-link">Contact Me</Link>
+        </h3>
+        <ul className="footer-link-container">
+          <li className="footer-link">Email: ch.fusco104@gmail.com</li>
+          <li className="footer-link">Phone: (319) 804-9186</li>
+          <li className="footer-link"><a href="https://www.linkedin.com/in/charlottefusco/" target="_blank" rel="noreferrer" className="linkedin">LinkedIn</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
