554f82a49e62814f314719b4fbb42968781b0fdd
nezcodin <ch.fusco104@gmail.com>
2023-04-26 19:44:18 -0500

    home buttons and about section

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,6 +1,17 @@
+import profilePicture from '../assets/profile.png'
+
 export const About = () => {
   return (
-    <div>
+    <div className='about-container'>
+      <div className='profile-picture'>
+        <img src={profilePicture} alt='profile' />
+      </div>
+      <div className='about-text'>
+        <h2>Hey, thanks for stopping by!</h2>
+        <p>My name is Charlotte Fusco. As a software engineer with a customer service background, I excel in providing creative solutions to complex problems while continuously learning and staying up-to-date with the latest trends in tech. My expertise in teamwork and customer-focused problem-solving allows me to effectively identify and resolve customer issues.</p>
+        <p>Languages and Frameworks: JavaScript | Python | HTML5 | CSS3 | SQL | React | Node.js | PostgreSQL | Express</p>
+        <p>Technologies: Git | GitHub | RESTful API | MacOS | Linux | Adobe After Effects | QuickBooks | Figma | Microsoft Excel</p>
+      </div>
     </div>
   )
 }
--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,6 +1,14 @@
+import { About } from "./About"
+
 export const Home = () => {
   return (
     <div>
+      <About />
+      <div className="buttons">
+        <button className="home-button">Check out my projects!</button>
+        <button className="home-button">Want my resume?</button>
+        <button className="home-button">Let's chat!</button>
+      </div>
     </div>
   )
 }
