c871ba60aada4d094ba3bd759696ab46da3ad5bb
nezcodin <ch.fusco104@gmail.com>
2023-04-29 02:54:09 -0500

    animeto done

--- portfolio/src/components/Animeto.js
+++ portfolio/src/components/Animeto.js
@@ -10,18 +10,31 @@ export const Animeto = () => {
   ]
 
   const screenshotImages = screenshots.map((screenshot, index) => (
-    <img key={index} src={screenshot} alt={`Screenshot ${index}`} />
+    <img key={index} src={screenshot} alt={`Screenshot ${index}`} className='screenshot' />
   ));
 
-
-
   return (
-    <div>
-      <h1>Animeto</h1>
-      <div>
-        {screenshotImages}
+    <div className='project-page'>
+      <h1 className='project-title'>Animeto</h1>
+      <div className='details-cont'>
+        <div className='details'>
+          <p>An anime search engine that helps users determine the next anime they should watch! Made possible using Jikan API.</p>
+          <ul>
+            <li>Anime search engine using Javascript, Jikan RESTful API, CSS, and HTML</li>
+            <li>Used Javascript to call the Jikan API to allow users to search for an anime and populate with data on the front end</li>
+            <li>Utilized CSS for styling and key framing to create custom loading animations </li>
+          </ul>
+          <button><a href='https://github.com/nezcodin/Animeto' target='_blank' rel="noreferrer">See Animeto Repository on GitHub</a></button>
+        </div>
       </div>
-      <div>
+      <div className='screenshots-cont'>
+        <h2>Screenshots</h2>
+        <div className='scroll-cont'>
+          {screenshotImages}
+        </div>
+      </div>
+      <div className='video-demo'>
+        <h2>Video Demo</h2>
         <video controls>
           <source src={animeto} type="video/mp4" />
         </video>
