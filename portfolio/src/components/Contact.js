a51dea0bc6765dce30ed39abb3c9288b1757bb2d
nezcodin <ch.fusco104@gmail.com>
2023-05-03 20:15:56 -0500

    contact page updated

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,34 +1,33 @@
 import emailjs from "emailjs-com"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 export const Contact = () => {
   const form = useRef();
+  const [buttonText, setButtonText] = useState("Send Email")
 
   const sendEmail = (e) => {
     e.preventDefault();
 
+    setButtonText("Sending...")
+
     emailjs.sendForm('service_5clewnu', 'portfolio-template', form.current, process.env.REACT_APP_API_PUBLIC_KEY)
       .then((result) => {
         console.log(result.text);
+        setButtonText("Sent!")
       }, (error) => {
         console.log(error.text);
+        setButtonText("Try again!")
       });
   };
 
   return (
     <div className="contact">
       <form ref={form} onSubmit={sendEmail} className="contact-box">
-        <div>
-          <label>Name</label>
-          <input type="text" name="user_name" className="subject" />
-          <label>Email</label>
-          <input type="email" name="user_email" className="subject" />
-          <label>Subject</label>
-          <input type="text" name="subject" className="subject" />
-        </div>
-        <label>Message</label>
-        <textarea name="message" className="message" />
-        <input type="submit" value="Send" />
+        <input type="text" name="user_name" className="contact-input" placeholder="Your Name" />
+        <input type="email" name="user_email" className="contact-input" placeholder="Your Email" />
+        <input type="text" name="subject" className="contact-input" placeholder="Subject" />
+        <textarea name="message" className="message" placeholder="What do you want to say?" />
+        <button type="submit">{buttonText}</button>
       </form>
 
       <p>Or send me a message over on <a href="https://www.linkedin.com/in/charlottefusco/" target="_blank" rel="noreferrer">LinkedIn</a>! Or send me a text here (319) 804-9186.</p>
