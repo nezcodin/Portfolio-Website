873172e38e9fbd06fd67f88100758c00d40d72dc
nezcodin <ch.fusco104@gmail.com>
2023-05-03 20:02:07 -0500

    contact form works!

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,12 +1,38 @@
+import emailjs from "emailjs-com"
+import { useRef } from "react"
+
 export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_5clewnu', 'portfolio-template', form.current, process.env.REACT_APP_API_PUBLIC_KEY)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <div className="contact">
-      <div className="contact-box">
-        <input placeholder="Subject" className="subject"></input>
-        <textarea placeholder="What do you want to say? Please mention your name and a good way to reach back out to you." className="message"></textarea>
-        <button type="submit">Send Email</button>
-      </div>
+      <form ref={form} onSubmit={sendEmail} className="contact-box">
+        <div>
+          <label>Name</label>
+          <input type="text" name="user_name" className="subject" />
+          <label>Email</label>
+          <input type="email" name="user_email" className="subject" />
+          <label>Subject</label>
+          <input type="text" name="subject" className="subject" />
+        </div>
+        <label>Message</label>
+        <textarea name="message" className="message" />
+        <input type="submit" value="Send" />
+      </form>
+
       <p>Or send me a message over on <a href="https://www.linkedin.com/in/charlottefusco/" target="_blank" rel="noreferrer">LinkedIn</a>! Or send me a text here (319) 804-9186.</p>
     </div>
-  )
+  );
 }
+
